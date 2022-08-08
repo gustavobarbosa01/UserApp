4b9af3602a14fa8202004e51885fa6b7545f551b
Gustavo Barbosa <gustavo.barbosabd@gmail.com>
2022-08-08 18:03:33 -0300

    Correção do Modulo zone.js@0.11.3 e Criação Classe Controller e Models.

---
+++ UserApp/Controllers/UserController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UserApp.Models;
+
+namespace UserApp.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserController : ControllerBase
+    {
+       
+       
+        private static List<User> usuarios = new List<User>();
+        private static int codigo = 1;
+
+        [HttpPost]
+        public IActionResult AddUser([FromBody] User usuario)
+        {
+            usuario.Codigo = codigo++;
+            usuarios.Add(usuario);
+
+            return CreatedAtAction(nameof(ListUserId), new { codigo = usuario.Codigo }, usuario);
+        }
+
+        [HttpGet]
+        public IActionResult ListUser()
+        {
+            return Ok(usuarios);
+        }
+
+        [HttpGet("{codigo}")]
+        public IActionResult ListUserId(int codigo)
+        {
+
+            User usuario = usuarios.FirstOrDefault(usuario => usuario.Codigo == codigo);
+
+            if (usuario != null)
+            {
+                return Ok(usuario);
+            }
+
+            return NotFound();
+        }
+    }
+}
---
+++ UserApp/Models/User.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UserApp.Models
+{
+    public class User
+    {
+		public int Codigo { get; set; }
+
+		[Required(ErrorMessage = "O campo Nome é Obrigatório!")]
+		[StringLength(50, ErrorMessage = "O campo Nome precisa ter no máximo 50 caracteres")]
+		public string Nome { get; set; }
+
+		[Required(ErrorMessage = "O campo CPF é Obrigatório!")]
+		[MinLength(11, ErrorMessage = "O campo CPF precisa ter no minimo 11 caracteres!")]
+		[MaxLength(11, ErrorMessage = "O campo CPF precisa ter no máximo 11 caracteres!")]
+		public string Cpf { get; set; }
+
+		public string Endereco { get; set; }
+
+		public string Telefone { get; set; }
+
+		public string Login { get; set; }
+
+		public string Senha { get; set; }
+	}
+}
